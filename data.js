ce1df1ed5fed6e413bb826966042147e208649c4
Dev-Rayenn <developertalibalilmry@gmail.com>
2024-04-13 19:54:27 +0100

    First Commit

---
+++ data.js
@@ -0,0 +1,52 @@
+
+export const songDataBase = [
+    {
+      songSrc: "./music/music1.mp3",
+      title: "ليس الغريب غريب الشام",
+      artist: "",
+      imgSrc: "./img/music1.jpg",
+    },
+    {
+      songSrc: "./music/music2.mp3",
+      title: "الرباعية",
+      artist: "",
+      imgSrc: "./img/music2.jpg",
+    },
+    {
+      songSrc: "./music/music3.mp3",
+      title: "تجميع",
+      artist: "",
+      imgSrc: "./img/music3.jpg",
+    },
+    {
+      songSrc: "./music/music4.mp3",
+      title: "المحبرة",
+      artist: "",
+      imgSrc: "./img/music4.jpg",
+    },
+    {
+      songSrc: "./music/music5.mp3",
+      title: "تائية الإمام الإلبيري",
+      artist: "",
+      imgSrc: "./img/music5.jpg",
+    },
+    {
+      songSrc: "./music/music6.mp3",
+      title: "السيف أصدق أنباءً من الكتب",
+      artist: "",
+      imgSrc: "./img/music6.jpg",
+    },
+    {
+      songSrc: "./music/music7.mp3",
+      title: "لمع البرق",
+      artist: "",
+      imgSrc: "./img/music7.jpg",
+    },
+    {
+      songSrc: "./music/music8.mp3",
+      title: "مثلث قطرب",
+      artist: "",
+      imgSrc: "./img/music8.jpg",
+    },
+    
+  ];
